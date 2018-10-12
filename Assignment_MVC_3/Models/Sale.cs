fc967c33553f05f11897c07e814bdb9a4f557487
Sergei Belialov <32887986+godszerg86@users.noreply.github.com>
2018-10-12 13:17:35 -0500

    Add project files.

---
+++ Assignment_MVC_3/Models/Sale.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_MVC_3.Models
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        public DateTimeOffset Date { get; set; }
+    }
+}
