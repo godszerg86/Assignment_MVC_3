39ae9ee090caec888cbf3c9368f6873341db7fa7
Sergei Belialov <32887986+godszerg86@users.noreply.github.com>
2018-10-12 14:31:03 -0500

    DONE. Seed method seeding tabels with basic info.

--- Assignment_MVC_3/Migrations/Configuration.cs
+++ Assignment_MVC_3/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace Assignment_MVC_3.Migrations
 {
+    using Assignment_MVC_3.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -18,6 +19,41 @@ namespace Assignment_MVC_3.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+
+            //seeding tables
+            if (!context.Products.Any())
+            {
+                context.Products.AddOrUpdate(item => item.Id,
+                    new Product() { Name = "Banana" , Quantity = 3, Price = 3.75m},
+                    new Product() { Name = "Apple", Quantity = 5, Price = 2.35m }
+                    );
+            }
+
+
+            if (!context.StoreLocations.Any())
+            {
+                context.StoreLocations.AddOrUpdate(item => item.Id,
+                    new StoreLocation() { LocationName = "Polo Park" },
+                    new StoreLocation() { LocationName = "Kenaston"}
+                    );
+            }
+
+            if (!context.Employees.Any())
+            {
+                context.Employees.AddOrUpdate(item => item.Id,
+                    new Employee() { Name = "John" , RegistrationNumber = 31234 },
+                    new Employee() { Name = "Dan", RegistrationNumber = 11223 }
+                    );
+            }
+
+            if (!context.Customers.Any())
+            {
+                context.Customers.AddOrUpdate(item => item.Id,
+                    new Customer() { Name = "Anna", Email = "anna@gmail.com" },
+                    new Customer() { Name = "Mary", Email = "mary@gmail.com" }
+                    );
+            }
         }
 
       
--- Assignment_MVC_3/Models/Employee.cs
+++ Assignment_MVC_3/Models/Employee.cs
@@ -17,7 +17,7 @@ namespace Assignment_MVC_3.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
-        public string RegistrationNumber { get; set; }
+        public int RegistrationNumber { get; set; }
 
         //relations
         [InverseProperty("EmployeeMakeSale")]
